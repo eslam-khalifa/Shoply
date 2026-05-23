23f13e391db2887edc5ee947bc8bee047998549c
eslam_khalifa <khalifaeslam754@gmail.com>
2026-05-24 02:55:25 +0300

    Seeding product brands and product types

---
+++ services/catalog/Catalog.Infrastructure/Data/Contexts/ProductBrandContextSeed.cs
@@ -0,0 +1,40 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data.Contexts
+{
+    public static class ProductBrandContextSeed
+    {
+        // sql server vs. mongodb
+        // database    database
+        // table       collection
+        // row         document
+        // column      field
+        public static async Task SeedDataAsync(IMongoCollection<ProductBrand> productBrandCollection)
+        {
+            // Find here means find all documents in the collection
+            var hasProductBrands = await productBrandCollection.Find(_ => true).AnyAsync();
+            if (hasProductBrands)
+                return;
+            var filePath = Path.Combine("Data", "SeededData", "brands.json");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File {filePath} does not exist.");
+                return;
+            }
+            var brandData = await File.ReadAllTextAsync(filePath);
+            var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+            if (brands?.Any() is true)
+            {
+                // InsertManyAsync is used to insert multiple documents into the collection at once, which is more efficient than inserting them one by one.
+                await productBrandCollection.InsertManyAsync(brands);
+            }
+        }
+    }
+}
---
+++ services/catalog/Catalog.Infrastructure/Data/Contexts/ProductTypeContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data.Contexts
+{
+    public static class ProductTypeContextSeed
+    {
+        public static async Task SeedDataAsync(IMongoCollection<ProductType> productTypeCollection)
+        {
+            var hasProductTypes = await productTypeCollection.Find(_ => true).AnyAsync();
+            if (hasProductTypes)
+                return;
+            var filePath = Path.Combine("Data", "SeededData", "types.json");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File {filePath} does not exist.");
+                return;
+            }
+            var productTypeData = await File.ReadAllTextAsync(filePath);
+            var productTypes = JsonSerializer.Deserialize<List<ProductType>>(productTypeData);
+            if (productTypes!.Any() is true)
+            {
+                await productTypeCollection.InsertManyAsync(productTypes);
+            }
+        }
+    }
+}
