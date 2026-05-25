ee89a4733d3bbcd13c78ba2ba1a25eec2f780cd5
eslam_khalifa <khalifaeslam754@gmail.com>
2026-05-26 01:47:12 +0300

    feat: implementing last product query and product query handler

---
+++ services/catalog/Catalog.Application/Handlers/Queries/GetAllProductsByBrandNameQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Queries
+{
+    public class GetAllProductsByBrandNameQueryHandler : IRequestHandler<GetAllProductsByBrandNameQuery, IList<ProductResponseDto>>
+    {
+        private readonly IProductRepository productRepository;
+
+        public GetAllProductsByBrandNameQueryHandler(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<IList<ProductResponseDto>> Handle(GetAllProductsByBrandNameQuery request, CancellationToken cancellationToken)
+        {
+            var products = await productRepository.GetAllByBrandNameAsync(request.Name);
+            return products.Select(p => p.ToProductResponseDto()).ToList();
+        }
+    }
+}
---
+++ services/catalog/Catalog.Application/Queries/GetAllProductsByBrandNameQuery.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Queries
+{
+    public class GetAllProductsByBrandNameQuery : IRequest<IList<ProductResponseDto>>
+    {
+        public string Name { get; }
+
+        public GetAllProductsByBrandNameQuery(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
